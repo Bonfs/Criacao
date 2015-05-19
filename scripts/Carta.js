77c24db69e48fd27a52f77b8b2b9ce8892c16e0c
Bonfs <matheusbonfim05@outlook.com>
2015-05-19 15:23:45 -0300

    Carta.js
    script responsavel pelo comportamento das cartas do jogo da memória.

---
+++ scripts/Carta.js
@@ -0,0 +1,61 @@
+function Carta()
+{}
+
+//Atributos
+Carta.prototype._marcada = false;        //verifica se a carta foi virad;
+Carta.prototype._carta = null;            //spritesheet
+Carta.prototype._carta_animation = null;  //sprite[back, front, flip, flipvolta]
+Carta.prototype._valor = null;            //valor da carta para comparação   
+Carta.prototype.x = null;
+Carta.prototype.y = null;
+
+
+//Métodos(não tem 'GET')
+Carta.prototype.setXY = function(array)
+{
+    if(typeof array == "object")
+    {
+        this.x = array[0];
+        this.y = array[1];
+    }
+    else
+        return undefined;
+}
+
+Carta.prototype.setValor = function(val)
+{
+    this._valor = val;
+}
+
+Carta.prototype.setMarcada = function(m)
+{
+    this._marcada = m;
+}
+
+Carta.prototype.setCarta = function(card)
+{
+    //card sera um objeto com o sprite sheet e todas as informações da animação
+    this._carta = new createjs.SpriteSheet(card);
+}
+
+Carta.prototype.setCartaAnimation = function()
+{
+    //animação default da carta será back('costas' da carta)
+    if(this._carta != null)
+    {
+        this._carta_animation = new createjs.Sprite(this._carta, "back");
+        this._carta_animation.pai = this;
+        this._carta_animation.x = this.x;
+        this._carta_animation.y = this.y;
+    }
+    else
+        return null;    
+}
+
+Carta.prototype.setAnimation = function()
+{
+        if(this._marcada)  
+            this._carta_animation.gotoAndPlay("flip");
+        else
+            this._carta_animation.gotoAndPlay("flipvolta");
+}
