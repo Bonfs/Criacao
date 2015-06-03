f1b897141cc5a8c68c200e7121a9a0d1beb4482b
Bonfs <matheusbonfim05@outlook.com>
2015-06-03 00:23:30 -0300

    Nova tela inicial
    Nova animação de tela inicial

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,48 @@ function carregarArquivos()
     //preload.installPlugin(createjs.Sound);
     preload.on("complete", init, this);
     preload.loadManifest([
+            {id: "tela_inicial0", src: "images/telaInicial/tela_inicial_0.png"},
+            {id: "tela_inicial1", src: "images/telaInicial/tela_inicial_1.png"},
+            {id: "tela_inicial2", src: "images/telaInicial/tela_inicial_2.png"},
+            {id: "tela_inicial3", src: "images/telaInicial/tela_inicial_3.png"},
+            {id: "tela_inicial4", src: "images/telaInicial/tela_inicial_4.png"},
+            {id: "tela_inicial5", src: "images/telaInicial/tela_inicial_5.png"},
+            {id: "tela_inicial6", src: "images/telaInicial/tela_inicial_6.png"},
+            {id: "tela_inicial7", src: "images/telaInicial/tela_inicial_7.png"},
+            {id: "tela_inicial8", src: "images/telaInicial/tela_inicial_8.png"},
+            {id: "tela_inicial9", src: "images/telaInicial/tela_inicial_9.png"},
+            {id: "tela_inicial10", src: "images/telaInicial/tela_inicial_10.png"},
+            {id: "tela_inicial11", src: "images/telaInicial/tela_inicial_11.png"},
+            {id: "tela_inicial12", src: "images/telaInicial/tela_inicial_12.png"},
+            {id: "tela_inicial13", src: "images/telaInicial/tela_inicial_13.png"},
+            {id: "tela_inicial14", src: "images/telaInicial/tela_inicial_14.png"},
+            {id: "tela_inicial15", src: "images/telaInicial/tela_inicial_15.png"},
+            {id: "tela_inicial16", src: "images/telaInicial/tela_inicial_16.png"},
+            {id: "tela_inicial17", src: "images/telaInicial/tela_inicial_17.png"},
+            {id: "tela_inicial18", src: "images/telaInicial/tela_inicial_18.png"},
+            {id: "tela_inicial19", src: "images/telaInicial/tela_inicial_19.png"},
+            {id: "tela_inicial20", src: "images/telaInicial/tela_inicial_20.png"},
+            {id: "tela_inicial21", src: "images/telaInicial/tela_inicial_21.png"},
+            {id: "tela_inicial22", src: "images/telaInicial/tela_inicial_22.png"},
+            {id: "tela_inicial23", src: "images/telaInicial/tela_inicial_23.png"},
+            {id: "tela_inicial24", src: "images/telaInicial/tela_inicial_24.png"},
+            {id: "tela_inicial25", src: "images/telaInicial/tela_inicial_25.png"},
+            {id: "tela_inicial26", src: "images/telaInicial/tela_inicial_26.png"},
+            {id: "tela_inicial27", src: "images/telaInicial/tela_inicial_27.png"},
+            {id: "tela_inicial28", src: "images/telaInicial/tela_inicial_28.png"},
+            {id: "tela_inicial29", src: "images/telaInicial/tela_inicial_29.png"},
+            {id: "tela_inicial30", src: "images/telaInicial/tela_inicial_30.png"},
+            {id: "tela_inicial31", src: "images/telaInicial/tela_inicial_31.png"},
+            {id: "tela_inicial32", src: "images/telaInicial/tela_inicial_32.png"},
+            {id: "tela_inicial33", src: "images/telaInicial/tela_inicial_33.png"},
+            {id: "tela_inicial34", src: "images/telaInicial/tela_inicial_34.png"},
+            {id: "tela_inicial35", src: "images/telaInicial/tela_inicial_35.png"},
+            {id: "tela_inicial36", src: "images/telaInicial/tela_inicial_36.png"},
+            {id: "tela_inicial37", src: "images/telaInicial/tela_inicial_37.png"},
+            {id: "tela_inicial38", src: "images/telaInicial/tela_inicial_38.png"},
+            {id: "tela_inicial39", src: "images/telaInicial/tela_inicial_39.png"},
+            {id: "tela_inicial40", src: "images/telaInicial/tela_inicial_40.png"},
+            {id: "tela_inicial41", src: "images/telaInicial/tela_inicial_41.png"},
             {id: "carta_metal", src: "images/carta_metal.png"},
             {id: "carta_organico", src: "images/carta_organico.png"},
             {id: "carta_papel", src: "images/carta_papel.png"},
@@ -44,7 +86,8 @@ function carregarArquivos()
             {id: "encaixe_carro", src: "images/carro.png"},
             {id: "encaixe_carro_sombra", src: "images/carro sombra.png"},
             {id: "encaixe_btn", src: "images/encaixe_btnVolta.png"},
-            {id: "sound_telaInicial", src: "sons/bg_telaInicial.mp3"},
+            {id: "sound_telaInicial", src: "sons/telainicialaudio.mp3"},
+            {id: "sound_telaInicial2", src: "sons/bg_telaInicial.mp3"},
             {id: "sound_sala1", src: "sons/musica_de_fundo3.mp3"},
             {id: "sound_sala2", src: "sons/musica_de_fundo2.mp3"},
             {id: "sound_sala3", src: "sons/musica_de_fundo1.mp3"},
@@ -52,7 +95,7 @@ function carregarArquivos()
             {id: "sound_trem", src: "sons/trenzinhoanimacao.mp3"},
             {id: "sound_memoriaFim", src: "sons/acerto_jogo_da memoria.mp3"},
             {id: "sound_encaixe", src: "sons/acerto_bau.mp3"},
-            {id: "sound_bolaQuicando", src: "sons/bolaQuicando.mp3"}
+            {id: "sound_bolaQuicando", src: "sons/bolaQuicando.mp3"},
         ]);
 }
 
@@ -71,15 +114,154 @@ function tick() {stage.update();}
 //Tela inical de play------------------*
 function tela_inicial()
 {
+    setTimeout(timer_telaInicial, 3500);
     var container = new createjs.Container();
-    createjs.Sound.play("sound_telaInicial",{loop:-1, volume: 0.6});
-    
+    createjs.Sound.play("sound_telaInicial",{volume: 0.6});
+    createjs.Sound.play("sound_telaInicial2",{volume: 0.4, delay: 3500, loop: -1});
+    var bgData = {
+            "framerate":24,
+            "images":[
+                preload.getResult("tela_inicial0"),
+                preload.getResult("tela_inicial1"),
+                preload.getResult("tela_inicial2"),
+                preload.getResult("tela_inicial3"),
+                preload.getResult("tela_inicial4"),
+                preload.getResult("tela_inicial5"),
+                preload.getResult("tela_inicial6"),
+                preload.getResult("tela_inicial7"),
+                preload.getResult("tela_inicial8"),
+                preload.getResult("tela_inicial9"),
+                preload.getResult("tela_inicial10"),
+                preload.getResult("tela_inicial11"),
+                preload.getResult("tela_inicial12"),
+                preload.getResult("tela_inicial13"),
+                preload.getResult("tela_inicial14"),
+                preload.getResult("tela_inicial15"),
+                preload.getResult("tela_inicial16"),
+                preload.getResult("tela_inicial17"),
+                preload.getResult("tela_inicial18"),
+                preload.getResult("tela_inicial19"),
+                preload.getResult("tela_inicial20"),
+                preload.getResult("tela_inicial21"),
+                preload.getResult("tela_inicial22"),
+                preload.getResult("tela_inicial23"),
+                preload.getResult("tela_inicial24"),
+                preload.getResult("tela_inicial25"),
+                preload.getResult("tela_inicial26"),
+                preload.getResult("tela_inicial27"),
+                preload.getResult("tela_inicial28"),
+                preload.getResult("tela_inicial29"),
+                preload.getResult("tela_inicial30"),
+                preload.getResult("tela_inicial31"),
+                preload.getResult("tela_inicial32"),
+                preload.getResult("tela_inicial33"),
+                preload.getResult("tela_inicial34"),
+                preload.getResult("tela_inicial35"),
+                preload.getResult("tela_inicial36"),
+                preload.getResult("tela_inicial37"),
+                preload.getResult("tela_inicial38"),
+                preload.getResult("tela_inicial39"),
+                preload.getResult("tela_inicial40"),
+                preload.getResult("tela_inicial41")
+            ],
+            "frames":[
+                [0, 0, 2048, 1024, 0, 0, 0],
+                [0, 1024, 2048, 1024, 0, 0, 0],
+                [0, 0, 2048, 1024, 1, 0, 0],
+                [0, 1024, 2048, 1024, 1, 0, 0],
+                [0, 0, 2048, 1024, 2, 0, 0],
+                [0, 1024, 2048, 1024, 2, 0, 0],
+                [0, 0, 2048, 1024, 3, 0, 0],
+                [0, 1024, 2048, 1024, 3, 0, 0],
+                [0, 0, 2048, 1024, 4, 0, 0],
+                [0, 1024, 2048, 1024, 4, 0, 0],
+                [0, 0, 2048, 1024, 5, 0, 0],
+                [0, 1024, 2048, 1024, 5, 0, 0],
+                [0, 0, 2048, 1024, 6, 0, 0],
+                [0, 1024, 2048, 1024, 6, 0, 0],
+                [0, 0, 2048, 1024, 7, 0, 0],
+                [0, 1024, 2048, 1024, 7, 0, 0],
+                [0, 0, 2048, 1024, 8, 0, 0],
+                [0, 1024, 2048, 1024, 8, 0, 0],
+                [0, 0, 2048, 1024, 9, 0, 0],
+                [0, 1024, 2048, 1024, 9, 0, 0],
+                [0, 0, 2048, 1024, 10, 0, 0],
+                [0, 1024, 2048, 1024, 10, 0, 0],
+                [0, 0, 2048, 1024, 11, 0, 0],
+                [0, 1024, 2048, 1024, 11, 0, 0],
+                [0, 0, 2048, 1024, 12, 0, 0],
+                [0, 1024, 2048, 1024, 12, 0, 0],
+                [0, 0, 2048, 1024, 13, 0, 0],
+                [0, 1024, 2048, 1024, 13, 0, 0],
+                [0, 0, 2048, 1024, 14, 0, 0],
+                [0, 1024, 2048, 1024, 14, 0, 0],
+                [0, 0, 2048, 1024, 15, 0, 0],
+                [0, 1024, 2048, 1024, 15, 0, 0],
+                [0, 0, 2048, 1024, 16, 0, 0],
+                [0, 1024, 2048, 1024, 16, 0, 0],
+                [0, 0, 2048, 1024, 17, 0, 0],
+                [0, 1024, 2048, 1024, 17, 0, 0],
+                [0, 0, 2048, 1024, 18, 0, 0],
+                [0, 1024, 2048, 1024, 18, 0, 0],
+                [0, 0, 2048, 1024, 19, 0, 0],
+                [0, 1024, 2048, 1024, 19, 0, 0],
+                [0, 0, 2048, 1024, 20, 0, 0],
+                [0, 1024, 2048, 1024, 20, 0, 0],
+                [0, 0, 2048, 1024, 21, 0, 0],
+                [0, 1024, 2048, 1024, 21, 0, 0],
+                [0, 0, 2048, 1024, 22, 0, 0],
+                [0, 1024, 2048, 1024, 22, 0, 0],
+                [0, 0, 2048, 1024, 23, 0, 0],
+                [0, 1024, 2048, 1024, 23, 0, 0],
+                [0, 0, 2048, 1024, 24, 0, 0],
+                [0, 1024, 2048, 1024, 24, 0, 0],
+                [0, 0, 2048, 1024, 25, 0, 0],
+                [0, 1024, 2048, 1024, 25, 0, 0],
+                [0, 0, 2048, 1024, 26, 0, 0],
+                [0, 1024, 2048, 1024, 26, 0, 0],
+                [0, 0, 2048, 1024, 27, 0, 0],
+                [0, 1024, 2048, 1024, 27, 0, 0],
+                [0, 0, 2048, 1024, 28, 0, 0],
+                [0, 1024, 2048, 1024, 28, 0, 0],
+                [0, 0, 2048, 1024, 29, 0, 0],
+                [0, 1024, 2048, 1024, 29, 0, 0],
+                [0, 0, 2048, 1024, 30, 0, 0],
+                [0, 1024, 2048, 1024, 30, 0, 0],
+                [0, 0, 2048, 1024, 31, 0, 0],
+                [0, 1024, 2048, 1024, 31, 0, 0],
+                [0, 0, 2048, 1024, 32, 0, 0],
+                [0, 1024, 2048, 1024, 32, 0, 0],
+                [0, 0, 2048, 1024, 33, 0, 0],
+                [0, 1024, 2048, 1024, 33, 0, 0],
+                [0, 0, 2048, 1024, 34, 0, 0],
+                [0, 1024, 2048, 1024, 34, 0, 0],
+                [0, 0, 2048, 1024, 35, 0, 0],
+                [0, 1024, 2048, 1024, 35, 0, 0],
+                [0, 0, 2048, 1024, 36, 0, 0],
+                [0, 1024, 2048, 1024, 36, 0, 0],
+                [0, 0, 2048, 1024, 37, 0, 0],
+                [0, 1024, 2048, 1024, 37, 0, 0],
+                [0, 0, 2048, 1024, 38, 0, 0],
+                [0, 1024, 2048, 1024, 38, 0, 0],
+                [0, 0, 2048, 1024, 39, 0, 0],
+                [0, 1024, 2048, 1024, 39, 0, 0],
+                [0, 0, 2048, 1024, 40, 0, 0],
+                [0, 1024, 2048, 1024, 40, 0, 0],
+                [0, 0, 2048, 1024, 41, 0, 0],
+                [0, 1024, 2048, 1024, 41, 0, 0]
+            ],
+            "animations":{
+                anima: [0, 81, false]
+              }
+            };
     var background = new createjs.Bitmap(preload.getResult("telaInicialBG"));
-    var play_inicial = new createjs.Bitmap(preload.getResult("btn-play"));
-    play_inicial.on("click", evtTela_inicial);
+    var backgroundFrames = new createjs.SpriteSheet(bgData);
+    var bg = new createjs.Sprite(backgroundFrames, "anima");
     
-    container.addChild(background);
-    container.addChild(play_inicial);
+    
+    
+    container.addChild(bg);
+    //container.addChild(play_inicial);
     
     return container;
 }
@@ -88,6 +270,15 @@ function evtTela_inicial(evt)
 {
     if(evt.type = "click")
         trocaTela(telaAvatar());
+}
+
+function timer_telaInicial()
+{
+    var play_inicial = new createjs.Bitmap(preload.getResult("btn-play"));
+    play_inicial.on("click", evtTela_inicial);
+    play_inicial.y = 1;
+    play_inicial.x = 1;
+    stage.addChild(play_inicial);
 }
 //------------------------------------*
 
@@ -142,7 +333,7 @@ function sala1()
     if(!msala1)
     {
         createjs.Sound.stop();
-        createjs.Sound.play("sound_sala1",{loop:-1, volume: 0.6});
+        createjs.Sound.play("sound_sala1",{loop:-1, volume: 0.8});
         msala1 = true;
     }
     var container = new createjs.Container();
